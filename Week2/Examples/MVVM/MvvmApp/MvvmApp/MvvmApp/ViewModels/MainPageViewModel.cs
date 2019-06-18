a92a9a6a48e2ad3a59041e1acefbe308bceb7e4f
Angel Garcia <angelrenegarcia13@gmail.com>
2019-06-18 13:40:49 -0400

    Added MVVM Example and Week's 2 Presentation.

---
+++ Week2/Examples/MVVM/MvvmApp/MvvmApp/MvvmApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MvvmApp.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public ICommand DisplayNameCommand { get; private set; }
+        public MainPageViewModel()
+        {
+            Name = "Here goes your name";
+            DisplayNameCommand = new Command(DisplayName);
+        }
+        
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var changed = PropertyChanged;
+            if (changed != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        string name = "";
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+        bool isBusy;
+        public bool IsBusy
+        {
+            get
+            {
+                return isBusy;
+            }
+            set
+            {
+                if (isBusy != value)
+                {
+                    isBusy = value;
+                    OnPropertyChanged("IsBusy");
+                }
+            }
+        }
+        public bool IsNotBusy
+        {
+            get
+            {
+                return !isBusy;
+            }
+            set
+            {
+                if (isBusy != value)
+                {
+                    OnPropertyChanged("IsNotBusy");
+                }
+            }
+        }
+        async void DisplayName()
+        {
+            IsBusy = true;
+            await Task.Delay(3000);
+            IsBusy = false;
+        }
+    }
+}
